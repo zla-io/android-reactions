4a5dd8f5ed265d5e3248c225d8f65050b4970bfa
shweta chauhan <shweta.c@simformsolutions.com>
2019-10-17 11:56:05 +0530

    feat :   - added reaction like facebook     - on single click like/unlike     - on long press open reaction dialog and set in button

--- sample/src/main/java/io/zla/reactions/sample/KotlinSamples.kt
+++ sample/src/main/java/io/zla/reactions/sample/KotlinSamples.kt
@@ -2,6 +2,8 @@ package io.zla.reactions.sample
 
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.AppCompatCheckBox
 import android.util.TypedValue
 import android.view.Gravity
 import android.view.View
@@ -19,9 +21,9 @@ fun MainActivity.setup() {
     // Popup DSL + listener via function
     val popup1 = reactionPopup(this, ::onReactionSelected) {
         reactions {
-            resId    { R.drawable.ic_crypto_btc }
-            resId    { R.drawable.ic_crypto_eth }
-            resId    { R.drawable.ic_crypto_ltc }
+            resId { R.drawable.ic_crypto_btc }
+            resId { R.drawable.ic_crypto_eth }
+            resId { R.drawable.ic_crypto_ltc }
             reaction { R.drawable.ic_crypto_dash scale ImageView.ScaleType.FIT_CENTER }
             reaction { R.drawable.ic_crypto_xrp scale ImageView.ScaleType.FIT_CENTER }
             drawable { getDrawable(R.drawable.ic_crypto_xmr) }
@@ -64,10 +66,64 @@ fun MainActivity.setup() {
         textVerticalPadding = 0
         textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 14f, resources.displayMetrics)
     }
-    val popup2 = ReactionPopup(this, config) { position -> true.also {
-        toast("$position selected")
-    } }
+    val popup2 = ReactionPopup(this, config) { position ->
+        true.also {
+            toast("$position selected")
+        }
+    }
     findViewById<View>(R.id.right_btn).setOnTouchListener(popup2)
+
+    /**
+     * Reaction like fb
+     */
+    val reactionArray = intArrayOf(R.drawable.ic_like, R.drawable.ic_in_love, R.drawable.ic_got_it, R.drawable.ic_need_it)
+    val reactionArrayColor = intArrayOf(R.color.color_like, R.color.color_love, R.color.color_got_it, R.color.orange)
+    val fbButton = findViewById<AppCompatCheckBox>(R.id.fb_btn)
+    val config3 = reactionConfig(this) {
+        reactionsIds = reactionArray
+        reactionTextProvider = { position -> strings[position] }
+        textBackground = ColorDrawable(Color.TRANSPARENT)
+        textColor = Color.BLACK
+        textHorizontalPadding = 0
+        textVerticalPadding = 0
+        textSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 14f, resources.displayMetrics)
+    }
+    val popup3 = ReactionPopup(this, config3) { position ->
+        true.also {
+            toast("$position selected")
+            if (position >= 0) {
+                fbButton.apply {
+                    isChecked = true
+                    text = strings[position]
+                    setTextColor(ContextCompat.getColor(context, reactionArrayColor[position]))
+                    buttonDrawable = context.getDrawable(reactionArray[position])
+                }
+            } else {
+                fbButton.apply {
+                    text = context.getString(R.string.txt_like)
+                    tag = context.getString(R.string.txt_like)
+                    setTextColor(ContextCompat.getColor(context, R.color.light_gray))
+                    buttonDrawable = context.getDrawable(R.drawable.ic_like_gray)
+                }
+            }
+        }
+    }
+
+    fbButton.setOnLongClickListener(popup3)
+
+    fbButton.setOnClickListener {
+        fbButton.apply {
+            buttonDrawable = context.getDrawable(R.drawable.bg_like_selector)
+
+            if (isChecked) {
+                text = context.getString(R.string.txt_like_it)
+                setTextColor(ContextCompat.getColor(context, R.color.color_like))
+            } else {
+                text = context.getString(R.string.txt_like)
+                setTextColor(ContextCompat.getColor(context, R.color.light_gray))
+            }
+        }
+    }
 }
 
 fun MainActivity.onReactionSelected(position: Int) = true.also {
